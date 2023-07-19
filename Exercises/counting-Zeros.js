503328bee26852c7e2731b6f17f44ecb8421fa61
igorntwari <igorntwari28@gmail.com>
2023-07-19 12:42:11 +0300

    solved Exercise 01

---
+++ Exercises/counting-Zeros.js
@@ -0,0 +1,16 @@
+let Searching = (n)=> {
+    let count = 0;
+  for (let i = 1; i <= n; i++) {
+    let numbr = i;
+    while (numbr > 0) {
+      if (numbr % 10 === 0) {
+        count++;
+      }
+     numbr = Math.floor(numbr / 10);
+    }
+  }
+
+  return count;
+}
+// console.log(Searching(50)); this are proved test case 
+// console.log(Searching(100)); this are proved test case
